cfbc6e46717640990c4255c9dcb9415ffa00d56b
JoshH831 <joshhowardspring@gmail.com>
2022-09-19 00:21:22 -0400

    Add rainbow and opacity functionality.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const gridContainer = document.querySelector('.gridContainer')
 const setWidthBtn = document.querySelector('#setWidthBtn')
 const clearBtn = document.querySelector('#clearBtn')
+const blackRadio = document.querySelector('#standardBlack')
+const rainbowRadio = document.querySelector('#rainbow')
+const opacityRadio = document.querySelector('#growingOpacity')
+const colorRadios = document.querySelectorAll('colorRadios')
+
 
 setWidthBtn.addEventListener('click', setNewWidth)
 clearBtn.addEventListener('click', clearGrid)
+blackRadio.addEventListener('click', changeColorMethod)
+rainbowRadio.addEventListener('click', changeColorMethod)
+opacityRadio.addEventListener('click', changeColorMethod)
+
+let colorType = 'hoverBlack'
 
 function createGrid(width, parentElement) {
     let newDiv;
@@ -21,8 +31,27 @@ function createGrid(width, parentElement) {
     }
 }
 
-function addHoverColor(element) {
-    this.classList.add('hoverColor')
+function addHoverColor(element, colorTypeV = colorType) {
+    switch(colorTypeV) {
+        case 'hoverBlack':
+            this.style['background-color'] ='black';
+            break;
+        case 'hoverRainbow':
+            this.style['background-color'] = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+            break;
+        case 'hoverOpacity':
+            if (this.classList.contains('hoverOpacity')) {
+                if (this.style.opacity < 1) {
+                    this.style.opacity = Number(this.style.opacity) + 0.1
+                }
+            } else {
+                this.style['background-color'] = 'black';
+                this.style.opacity = 0.1
+            }
+            break;            
+    }
+   this.classList.add(colorTypeV)    
+    
 }
 
 function setNewWidth() {
@@ -43,11 +72,22 @@ function setNewWidth() {
 }
 
 function clearGrid() {
-    let coloredBoxes = document.querySelectorAll('.hoverColor')
 
-    coloredBoxes.forEach(box => {
-        box.classList.remove('hoverColor')
+    const colorOptionClasses = ['hoverBlack', 'hoverRainbow', 'hoverOpacity']
+
+    colorOptionClasses.forEach(option => {
+        let coloredBoxes = document.querySelectorAll(`.${option}`)
+        coloredBoxes.forEach(box => {
+            box.classList.remove(`${option}`)
+            box.style['background-color'] = `white`;
+            box.style.opacity = 1;
+        })       
     })
+}
+
+function changeColorMethod() {
+    clearGrid()
+    colorType = this.value;
 }
 
 createGrid(16, gridContainer)
